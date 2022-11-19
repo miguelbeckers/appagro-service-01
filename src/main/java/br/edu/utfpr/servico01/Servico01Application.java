7e36bf2c89ef0fa9f74ed54d7389ad051c3f29d9
miguelbeckers <52552717+miguelbeckers@users.noreply.github.com>
2022-11-19 19:22:28 -0300

    configuracao inicial

---
+++ src/main/java/br/edu/utfpr/servico01/Servico01Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.servico01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servico01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servico01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/servico01/Servico01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.servico01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servico01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
