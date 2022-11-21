5ae8ecd56583ba6b868319e26287181eb88da248
miguelbeckers <52552717+miguelbeckers@users.noreply.github.com>
2022-11-21 03:27:38 -0300

    removidos as importações

--- src/main/java/br/edu/utfpr/servico01/model/Area.java
+++ src/main/java/br/edu/utfpr/servico01/model/Area.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
